d5eb58f404ce6c58385c01addb74ea0d88ff935a
Amit <akumar00029@gmail.com>
2022-07-24 08:50:15 +0530

    ip trace | ip  info added

--- cmd/trace.go
+++ cmd/trace.go
@@ -5,8 +5,13 @@ Copyright © 2022 NAME HERE <akumar00029@gmail.com>
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 )
 
@@ -20,10 +25,79 @@ var traceCmd = &cobra.Command{
 			fmt.Println("Please provide IP to trace")
 			return
 		}
-		fmt.Println(args)
+		for _, ip := range args {
+			showData(ip)
+			fmt.Println("------------------------------------------------")
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(traceCmd)
+}
+
+type IP struct {
+	IP       string `json:"ip"`
+	Hostname string `json:"hostname"`
+	Anycast  bool   `json:"anycast"`
+	City     string `json:"city"`
+	Region   string `json:"region"`
+	Country  string `json:"country"`
+	Loc      string `json:"loc"`
+	Org      string `json:"org"`
+	Postal   string `json:"postal"`
+	Timezone string `json:"timezone"`
+	Readme   string `json:"readme"`
+}
+
+func showData(ip string) {
+	url := "http://ipinfo.io/" + ip + "/geo"
+
+	responseBytes := getData(url)
+
+	data := &IP{}
+
+	err := json.Unmarshal(responseBytes, &data)
+
+	if err != nil {
+		log.Println("Unable to unmarshal response")
+		return
+	}
+
+	c := color.New(color.FgRed).Add(color.Bold)
+
+	c.Println(":::::::::::::::::::::::::::")
+	c.Println(":::     DATA Found      :::")
+	c.Println(":::::::::::::::::::::::::::")
+
+	fmt.Println("IP       : " + data.IP)
+	fmt.Println("Hostname : " + data.Hostname)
+	fmt.Println("City     : " + data.City)
+	fmt.Println("Region   : " + data.Region)
+	fmt.Println("Country  : " + data.Country)
+	fmt.Println("Loc      : " + data.Loc)
+	fmt.Println("Org      : " + data.Org)
+	fmt.Println("Postal   : " + data.Postal)
+	fmt.Println("Timezone : " + data.Timezone)
+	fmt.Println("Readme   : " + data.Readme)
+}
+
+func getData(url string) []byte {
+
+	response, err := http.Get(url)
+
+	if err != nil {
+		log.Println("Unable to get response")
+		return nil
+	}
+
+	responseBytes, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		log.Println("Unable to read response")
+		return nil
+	}
+
+	return responseBytes
+
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/AmitSureshChandra/iptracker
 
 go 1.13
 
-require github.com/spf13/cobra v1.5.0
+require (
+	github.com/fatih/color v1.13.0
+	github.com/spf13/cobra v1.5.0
+)
